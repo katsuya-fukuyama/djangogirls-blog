f2dfa09228d7fe07d015b27ea7f94a0b447cf6db
user.email <58612851+katsuya-fukuyama@users.noreply.github.com>
2020-02-24 12:27:30 +0900

    debug=false

--- girlsblog/settings.py
+++ girlsblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&ynk)pb*j6q=ir$p#(s=l9fm-2-0ndnh-x)dn1rxhzfk*e($df'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
